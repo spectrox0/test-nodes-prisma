af46abd6b1c22dba468b3d3dc4d88e69bf57df5f
Alejandro Velazco <alejanvelazco2008@hotmail.com>
2023-08-31 12:58:24 -0300

    feature: include readme.md

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 reset:
 	rm -rf node_modules pnpm-lock.yaml ; pnpm i ; pnpm build ; docker compose up --build -d
-up:
-	docker compose up --build -up
+up-dev:
+	docker compose up --build -d
 migrate:
 	docker exec test_node_api bash -c "npx prisma db push --accept-data-loss && npx prisma db pull && pnpm build";
+env-sample:
+	cat .env.example > .env
--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,18 @@ import { config } from "config";
 import helmet from "helmet";
 import cors from "cors";
 import * as Routes from "routes";
+//Instance of express
 const app = express();
 //Include all routes
 const routes = Object.values(Routes);
+
+//Base api name
 const baseApiName = "/api";
 app.use(baseApiName, routes);
 
 //Middlewares to parse the request body
-[express.json, helmet, cors].forEach((middleware) => app.use(middleware()));
+[express.json, helmet, cors].forEach(middleware => app.use(middleware()));
 
-app.listen(config.PORT);
+app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`);
+});
